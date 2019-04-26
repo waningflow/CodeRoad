931ac95a3345bfde02be34d83d38a3e77d0686a5
waningflow <jack1995woods@gmail.com>
2019-04-26 22:46:17 +0800

    update cli

--- cli/index.js
+++ cli/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const { up } = require('../server/index')
 
 program
   .option('-p, --port [value]')
@@ -8,3 +9,9 @@ program
 
 console.log('run')
 console.log(program.port)
+
+up()
+
+let url = 'http://localhost:3450'
+let start = (process.platform === 'darwin'? 'open': process.platform === 'win32'? 'start': 'xdg-open')
+require('child_process').exec(start + ' ' + url)
--- server/index.js
+++ server/index.js
@@ -1,44 +1,57 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const cors = require('@koa/cors')
+const serve = require('koa-static')
 // const dirTree = require('directory-tree')
 const { getDepcruise } = require('../util')
 const fs = require('fs')
+const path = require('path')
 
-const app = new Koa()
-const router = new Router()
+function up() {
+  const app = new Koa()
+  const router = new Router()
 
-// const tree = dirTree('/Users/waning/Pinssible/github/vue/src')
-// console.log(tree)
-app.use(cors())
+  // const tree = dirTree('/Users/waning/Pinssible/github/vue/src')
+  // console.log(tree)
+  app.use(cors())
 
-// router.get('/dirtree', (ctx, next) => {
-//   ctx.body = tree
-// })
+  // router.get('/dirtree', (ctx, next) => {
+  //   ctx.body = tree
+  // })
 
-router.get('/depcruise', (ctx, next) => {
-  try{
-    ctx.body = getDepcruise({
-      rootPath: '/Users/waning/Pinssible/github/vue/src',
-      aliasPath: '/Users/waning/Pinssible/github/vue/scripts/alias'
-    })
-    // ctx.body = getDepcruise({
-    //   rootPath: '/Users/waning/Pinssible/github/react/packages'
-    // })
-  }catch(e){
-    next(e)
-  }
-})
+  router.get('/depcruise', (ctx, next) => {
+    try {
+      ctx.body = getDepcruise({
+        rootPath: '/Users/waning/Pinssible/github/vue/src',
+        aliasPath: '/Users/waning/Pinssible/github/vue/scripts/alias'
+      })
+      // ctx.body = getDepcruise({
+      //   rootPath: '/Users/waning/Pinssible/github/react/packages'
+      // })
+    } catch (e) {
+      next(e)
+    }
+  })
 
-router.get('/file', (ctx, next) => {
-  try{
-    let content = fs.readFileSync(ctx.query.filepath)
-    ctx.body = content
-  }catch(e){
-    next(e)
-  }
-})
+  router.get('/file', (ctx, next) => {
+    try {
+      let content = fs.readFileSync(ctx.query.filepath)
+      ctx.body = content
+    } catch (e) {
+      next(e)
+    }
+  })
 
-app.use(router.routes())
+  app.use(serve(path.resolve(__dirname, '../client/dist/')) )
+  app.use(router.routes())
 
-app.listen(3450)
+  app.listen(3450)
+}
+
+module.exports = {
+  up
+}
+
+if (!module.parent) {
+  up()
+}

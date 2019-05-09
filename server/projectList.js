8d7781d3c8ee743d173df1779cfd3c1fd16c5bbe
waningflow <jack1995woods@gmail.com>
2019-05-09 13:39:27 +0800

    update projectlist

--- server/projectList.js
+++ server/projectList.js
@@ -14,5 +14,40 @@ module.exports = [
     name: 'vuex',
     src: './src',
     repo: 'git@github.com:vuejs/vuex.git'
+  },
+  {
+    name: 'react-redux',
+    src: './src',
+    repo: 'git@github.com:reduxjs/react-redux.git'
+  },
+  {
+    name: 'redux',
+    src: './src',
+    repo: 'git@github.com:reduxjs/redux.git'
+  },
+  {
+    name: 'vue-router',
+    src: './src',
+    repo: 'git@github.com:vuejs/vue-router.git'
+  },
+  {
+    name: 'react-router',
+    src: './packages',
+    repo: 'git@github.com:ReactTraining/react-router.git'
+  },
+  {
+    name: 'axios',
+    src: './lib',
+    repo: 'git@github.com:axios/axios.git'
+  },
+  {
+    name: 'express',
+    src: './lib',
+    repo: 'git@github.com:expressjs/express.git'
+  },
+  {
+    name: 'koa',
+    src: './lib',
+    repo: 'git@github.com:koajs/koa.git'
   }
 ]
